71f1006f558a0b594ffd1faef0359cdb277dcf63
tanyadhillon <tanyadhillan@gmail.com>
2026-06-01 21:21:33 +0530

    find factorial of a number

---
+++ factorial.c
@@ -0,0 +1,19 @@
+// find the factorial of a number
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d = %lld\n", n, factorial);
+
+    return 0;
+}
