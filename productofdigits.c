c0e86d6a99ffffb774fbec1569e5c2ca2b75ef52
tanyadhillon <tanyadhillan@gmail.com>
2026-06-02 20:29:50 +0530

    Add program to find product of digits
    Implement a program to calculate the product of digits of a given number.

---
+++ productofdigits.c
@@ -0,0 +1,19 @@
+// To find product of digits
+
+#include <stdio.h>
+
+int main() {
+    int num, product = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        product *= num % 10;
+        num /= 10;
+    }
+
+    printf("Product of digits = %d", product);
+
+    return 0;
+}
