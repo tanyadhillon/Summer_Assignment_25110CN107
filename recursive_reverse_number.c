6de9787aaeaf78bd080271857aeeefb6ea359030
tanyadhillon <tanyadhillan@gmail.com>
2026-06-07 14:14:41 +0530

    Add program to recursive reverse number
    Implement recursive function to reverse a number.

---
+++ recursive_reverse_number.c
@@ -0,0 +1,22 @@
+// To recursive reverse number
+
+#include <stdio.h>
+
+int reverse = 0;
+
+int reverseNumber(int n) {
+    if (n == 0)
+        return reverse;
+
+    reverse = reverse * 10 + (n % 10);
+    return reverseNumber(n / 10);
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Reversed number = %d", reverseNumber(n));
+    return 0;
+}
