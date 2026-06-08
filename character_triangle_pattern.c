c7380c98ca0c1b201e56ca0ec0451649e25a5446
tanyadhillon <tanyadhillan@gmail.com>
2026-06-08 18:51:10 +0530

    Add character triangle pattern printing program
    This program prints a triangle pattern of characters from 'A' to 'E'.

---
+++ character_triangle_pattern.c
@@ -0,0 +1,22 @@
+/* print character triangle
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j <= i; j++) {
+            printf("%c", 'A' + j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
