27a3cb69b12a616f730fe31eeb09f73bcf87b931
tanyadhillon <tanyadhillan@gmail.com>
2026-06-09 20:46:02 +0530

    Add reverse star pattern printing program
    This program prints a reverse star pattern, starting from five stars down to one.

---
+++ reverse_star_pattern.c
@@ -0,0 +1,22 @@
+/* print reverse star pattern 
+*****
+****
+***
+**
+*
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 5; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
