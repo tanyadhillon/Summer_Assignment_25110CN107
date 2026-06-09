846cf9b2c06e3621c307ecf736a96e10c6364e9b
tanyadhillon <tanyadhillan@gmail.com>
2026-06-09 20:54:34 +0530

    Add repeated character pattern printing program
    Implement a program to print a repeated character pattern from 'A' to 'E'.

---
+++ repeated_character_pattern.c
@@ -0,0 +1,24 @@
+/* print repeated character pattern 
+A
+BB
+CCC
+DDDD
+EEEEE
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    char ch = 'A';
+
+    for (i = 1; i <= 5; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%c", ch);
+        }
+        printf("\n");
+        ch++;
+    }
+
+    return 0;
+}
