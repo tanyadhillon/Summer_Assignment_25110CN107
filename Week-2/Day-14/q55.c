ac3f61492d58935c298af8b5bd766537c70696a4
tanyadhillon <tanyadhillan@gmail.com>
2026-06-16 20:51:36 +0530

    Organize repository into week-wise and day-wise folders

---
+++ Week-2/Day-14/q55.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[100], i;
+    int largest, secondLargest;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = secondLargest = -2147483648;
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    printf("Second largest element = %d\n", secondLargest);
+
+    return 0;
+}
