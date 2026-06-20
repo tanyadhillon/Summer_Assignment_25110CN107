f2cd7d6287c46ee3917a1b60cf328f4edf32df0a
tanya dhillon <25110cn107@glbitm.ac.in>
2026-06-20 16:53:30 +0530

    Organize repository into week-wise and day-wise folders

--- Week-3/Day-18/q69.c
+++ Week-3/Day-18/q69.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main() {
@@ -26,32 +25,3 @@ int main() {
 
     return 0;
 }
-=======
-#include <stdio.h>
-
-int main() {
-    int n, i, j, temp;
-
-    scanf("%d", &n);
-    int arr[n];
-
-    for(i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    for(i = 0; i < n - 1; i++) {
-        for(j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    printf("Sorted Array: ");
-    for(i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-
-    return 0;
-}
->>>>>>> c1a6f03 (Organize repository into week-wise and day-wise folders)
