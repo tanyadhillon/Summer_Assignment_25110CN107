337b047d5dcf3457ec3f107fd9cf4bc277e34795
tanya dhillon <25110cn107@glbitm.ac.in>
2026-06-25 21:26:37 +0530

    Organize repository into week-wise and day-wise folders

---
+++ Week-4/Day-25/q97.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[50], b[50], c[100];
+    int n1, n2, i=0, j=0, k=0;
+
+    printf("Enter size of first array: ");
+    scanf("%d",&n1);
+
+    printf("Enter first sorted array: ");
+    for(i=0;i<n1;i++)
+        scanf("%d",&a[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d",&n2);
+
+    printf("Enter second sorted array: ");
+    for(i=0;i<n2;i++)
+        scanf("%d",&b[i]);
+
+    i=0;
+
+    while(i<n1 && j<n2)
+    {
+        if(a[i] < b[j])
+            c[k++] = a[i++];
+        else
+            c[k++] = b[j++];
+    }
+
+    while(i<n1)
+        c[k++] = a[i++];
+
+    while(j<n2)
+        c[k++] = b[j++];
+
+    printf("Merged array: ");
+
+    for(i=0;i<k;i++)
+        printf("%d ",c[i]);
+
+    return 0;
+}
---
+++ Week-4/Day-25/q99.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[50][50], temp[50];
+    int n,i,j;
+
+    printf("Enter number of names: ");
+    scanf("%d",&n);
+
+    printf("Enter names:\n");
+
+    for(i=0;i<n;i++)
+        scanf("%s",name[i]);
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(strcmp(name[i],name[j]) > 0)
+            {
+                strcpy(temp,name[i]);
+                strcpy(name[i],name[j]);
+                strcpy(name[j],temp);
+            }
+        }
+    }
+
+    printf("Names in alphabetical order:\n");
+
+    for(i=0;i<n;i++)
+        printf("%s\n",name[i]);
+
+    return 0;
+}
